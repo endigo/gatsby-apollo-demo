a51f6dfa3e26ed25e632faa3f10f77a15eb717f8
endigo <tsogt.otgonbaatar@gmail.com>
2019-07-06 10:38:33 +0800

    rearrange query

--- src/templates/product.tsx
+++ src/templates/product.tsx
@@ -14,22 +14,6 @@ interface ProductProps {
   };
 }
 
-const PRODUCT_QUERY = gql`
-  subscription ProductSubscription($id: Int!) {
-    product: products_by_pk(id: $id) {
-      id
-      name
-      description
-      price
-      reviews {
-        id
-        comment
-        created_at
-      }
-    }
-  }
-`;
-
 const ProductPage = (props: ProductProps) => {
   const { product } = props.data.hasura;
   const { data } = useSubscription(PRODUCT_QUERY, {
@@ -56,6 +40,22 @@ const ProductPage = (props: ProductProps) => {
 };
 
 export default ProductPage;
+
+export const PRODUCT_QUERY = gql`
+  subscription ProductSubscription($id: Int!) {
+    product: products_by_pk(id: $id) {
+      id
+      name
+      description
+      price
+      reviews {
+        id
+        comment
+        created_at
+      }
+    }
+  }
+`;
 
 export const pageQuery = graphql`
   query TemplateProduct($id: Int!) {
